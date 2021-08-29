c3a43046596eb281f0f6b020d03420ae357921d4
bthank <bthankachan@yahoo.com>
2021-08-29 19:41:23 -0400

    Changes to have Webpack display products to HTML page

--- products/src/index.js
+++ products/src/index.js
@@ -2,11 +2,11 @@ import faker from 'faker'; // the faker library is used to generate fake data
 
 let products = '';
 
-for (let i=0; i < 3; i++){
+for (let i=0; i < 5; i++){
     const name = faker.commerce.productName(); // create a fake product name using faker library
 
-    product += `<div>${name}</div>`;
+    products += `<div>${name}</div>`;
 
 }
 
-console.log(products);
+document.querySelector('#dev-products').innerHTML = products;
